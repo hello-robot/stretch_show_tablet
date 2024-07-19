1b9fd635e865e9c6c7835092d0a3b6f5ff85b5f2
Matt Lamsey <lamsey@hello-robot.com>
2024-07-19 17:42:37 -0400

    Amaln/fixes from testing (#16)
    * Spin in background and catch KeyboardInterrupt to process termination
    
    * Added toggle service to togglebale_detection_node
    
    * Fix toggle logger bug
    
    ---------
    
    Co-authored-by: Amal Nanavati <amaln@hello-robot.com>

--- stretch_tablet/stretch_tablet/demo_show_tablet_actions.py
+++ stretch_tablet/stretch_tablet/demo_show_tablet_actions.py
@@ -1,4 +1,5 @@
 import rclpy
+from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 from rclpy.action import ActionClient, GoalResponse, CancelResponse
 from rclpy.timer import Rate
@@ -74,10 +75,6 @@ class DemoShowTablet(Node):
         self._feedback_show_tablet = ShowTablet.Feedback()
         self._feedback_track_head = TrackHead.Feedback()
 
-        # threads for UI
-        self.ui_thread = threading.Thread(target=self.main)
-        self.ui_thread.start()
-
     # callbacks
     def callback_show_tablet_feedback(self, feedback: ShowTablet.Feedback):
         self._feedback_show_tablet = feedback
@@ -126,7 +123,7 @@ class DemoShowTablet(Node):
         self.get_logger().info("DemoShowTablet: Exiting!")
         return DemoState.EXIT
 
-    def main(self):
+    def run(self):
         state = DemoState.IDLE
 
         self.rate = self.create_rate(self._wait_rate_hz)
@@ -154,10 +151,29 @@ class DemoShowTablet(Node):
 def main():
     rclpy.init()
     node = DemoShowTablet()
-    rclpy.spin(node)
-    node.ui_thread.join()
+    executor = MultiThreadedExecutor(num_threads=4)
+
+    # Spin in the background since detecting faces will block
+    # the main thread
+    spin_thread = threading.Thread(
+        target=rclpy.spin,
+        args=(node,),
+        kwargs={"executor": executor},
+        daemon=True,
+    )
+    spin_thread.start()
+
+    # Run face detection
+    try:
+        node.run()
+    except KeyboardInterrupt:
+        pass
+
+    # Terminate this node
     node.destroy_node()
     rclpy.shutdown()
+    # Join the spin thread (so it is spinning in the main thread)
+    spin_thread.join()
 
 if __name__ == '__main__':
     main()
