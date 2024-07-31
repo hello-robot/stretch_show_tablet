bc01b078bf2702151c3d8e1240992309ffd38bf9
hello-lamsey <lamsey@hello-robot.com>
2024-07-31 17:45:10 -0400

    checkin before switch to fix something else

--- stretch_show_tablet/stretch_show_tablet/demo_show_tablet_action.py
+++ stretch_show_tablet/stretch_show_tablet/demo_show_tablet_action.py
@@ -7,11 +7,7 @@ from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 from rclpy.timer import Rate
 
-from stretch_show_tablet_interfaces.action import (
-    EstimateHumanPose,
-    ShowTablet,
-    TrackHead,
-)
+from stretch_show_tablet_interfaces.action import ShowTablet
 
 
 def run_action(
@@ -40,57 +36,37 @@ class DemoState(Enum):
     IDLE = 0
     # ESTIMATE_POSE = 1
     SHOW_TABLET = 2
-    TRACK_HEAD = 3
     EXIT = 99
 
 
 class DemoShowTablet(Node):
     def __init__(self):
         super().__init__("demo_show_tablet")
+
         # actions
-        self.act_estimate_pose = ActionClient(
-            self,
-            EstimateHumanPose,
-            "estimate_human_pose",
-        )
         self.act_show_tablet = ActionClient(
             self,
             ShowTablet,
             "show_tablet",
         )
-        self.act_track_head = ActionClient(
-            self,
-            TrackHead,
-            "track_head",
-        )
 
         # wait for servers
         _wait_time = 10.0
-        for act in [self.act_show_tablet, self.act_track_head]:
-            if not act.wait_for_server(_wait_time):
-                self.get_logger().error(
-                    "DemoShowTablet::init: did not find action servers, exiting..."
-                )
-                rclpy.shutdown()
+        if not self.act_show_tablet.wait_for_server(_wait_time):
+            self.get_logger().error(
+                "DemoShowTablet::init: did not find action server, exiting..."
+            )
+            rclpy.shutdown()
 
         # config
         self._wait_rate_hz = 10.0
 
-        # state
-        self._body_pose_estimate = None
-        self._camera_pose = None
-
         # feedback
-        self._feedback_estimate_pose = EstimateHumanPose.Feedback()
         self._feedback_show_tablet = ShowTablet.Feedback()
-        self._feedback_track_head = TrackHead.Feedback()
 
     # callbacks
     def callback_show_tablet_feedback(self, feedback: ShowTablet.Feedback):
         self._feedback_show_tablet = feedback
-
-    def callback_track_head_feedback(self, feedback: TrackHead.Feedback):
-        self._feedback_track_head = feedback
 
     # states
     def state_idle(self) -> DemoState:
@@ -108,14 +84,9 @@ class DemoShowTablet(Node):
             return DemoState.IDLE
 
     def state_show_tablet(self) -> DemoState:
-        # if self._body_pose_estimate is None:
-        #     self.get_logger().warn("DemoShowTablet::state_show_tablet: body estimate is None!")
-        #     return DemoState.ESTIMATE_POSE
-
         # send request
         request = ShowTablet.Goal()
-        # request.human_joint_dict = json.dumps(self._body_pose_estimate)
-        # request.camera_pose = self._camera_pose
+        request.number_of_pose_estimates = 10
 
         result = run_action(  # noqa: F841
             self.act_show_tablet,
@@ -124,20 +95,7 @@ class DemoShowTablet(Node):
             feedback_callback=self.callback_show_tablet_feedback,
         )
 
-        return DemoState.TRACK_HEAD
-
-    def state_track_head(self) -> DemoState:
-        # send request
-        request = TrackHead.Goal()
-
-        result = run_action(  # noqa: F841
-            self.act_track_head,
-            request,
-            self.rate,
-            feedback_callback=self.callback_track_head_feedback,
-        )
-
-        return DemoState.EXIT
+        return DemoState.IDLE
 
     def state_exit(self) -> DemoState:
         self.get_logger().info("DemoShowTablet: Exiting!")
@@ -152,12 +110,8 @@ class DemoShowTablet(Node):
             self.get_logger().info("Current State: " + str(state))
             if state == DemoState.IDLE:
                 state = self.state_idle()
-            # elif state == DemoState.ESTIMATE_POSE:
-            #     state = self.state_estimate_human_pose()
             elif state == DemoState.SHOW_TABLET:
                 state = self.state_show_tablet()
-            elif state == DemoState.TRACK_HEAD:
-                state = self.state_track_head()
             elif state == DemoState.EXIT:
                 state = self.state_exit()
                 break
